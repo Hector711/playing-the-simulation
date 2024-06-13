de364af47ad042ab39a25794906e5bd49a95d066
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-06-13 23:38:42 +0200

    feat: :beers:

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,12 +1,47 @@
 import React, { useState } from 'react';
 import Close from '@/icons/Close';
+import Poll from '@/icons/Poll';
+import Clip from '@/icons/Clip';
+import Gif from '@/icons/Gif';
+
+import Select from 'react-select';
+
+
 
 export default function NewPost() {
-  const [state, setState] = useState(false);
+  const [state, setState] = useState(true);
 
   const toggleNewPostForm = () => {
     setState(prevState => !prevState);
   };
+
+  const colourStyles = {
+    control: styles => ({ ...styles, backgroundColor: '#1a1e23'}),
+    option: styles => ({
+      ...styles,
+      backgroundColor: '#1a1e23',
+      ':active': { ...styles[':active'], backgroundColor: '#565656de' },
+    }),
+    multiValue: styles => ({ ...styles, backgroundColor: '#f6921b' }),
+    multiValueLabel: styles => ({ ...styles, color: '#111822' }),
+  };
+
+  const category = [
+    { value: 'win', label: '🏆 WIN' },
+    { value: 'aportacion', label: '👔 Aportación' },
+    { value: 'pitch', label: '🛗 Pitch Deck' },
+    { value: 'afterwork', label: '🍺 Afterwork' }
+  ];
+
+  const labels = [
+    { value: 'informe', label: 'Informe Ejecución' },
+    { value: 'programacion', label: 'Programacion' },
+    { value: 'productividad', label: 'Productividad' },
+    { value: 'marketing', label: 'Marketing' },
+    { value: 'habilidades-sociales', label: 'Habilidades Sociales' },
+    { value: 'finanzas', label: 'Finanzas y Fiscalidad' },
+  ];
+
   return (
     <div id='new-post' className={state ? 'expanded' : 'collapsed'}>
       {!state ? (
@@ -26,22 +61,42 @@ export default function NewPost() {
             </button>
           </div>
           <form>
-            <input type='text' placeholder='Title' id='title' />
-            <input type='text' placeholder='Write Something' id='description' />
+            <input type='text' placeholder='Tiulo' id='title' />
+            <input type='text' placeholder='Descripción' id='description' />
+            <div id='options'>
+              <button>
+                <Clip />
+              </button>
+              <button>
+                <Poll />
+              </button>
+              <button>
+                <Gif />
+              </button>
+            </div>
+            <div id='selects'>
+              <Select
+                options={category}
+                className='selects'
+                id='category'
+                styles={colourStyles}
+                placeholder='Categoría'
+              />
+              <Select
+                closeMenuOnSelect={false}
+                defaultValue='Etiquetas'
+                isMulti
+                options={labels}
+                styles={colourStyles}
+                className='selects'
+                id='label'
+                placeholder='Etiquetas'
+              />
+            </div>
+            <button>Send</button>
           </form>
         </>
       )}
     </div>
   );
 }
-
-/* <div>
-<button>Adj</button>
-<button>Link</button>
-<button>YT</button>
-<button>Poll</button>
-<button>Emoji</button>
-<button>Gif</button>
-<button>Cat</button>
-<button>Send</button>
-</div> */
---
+++ src/icons/Poll.jsx
@@ -0,0 +1,22 @@
+import * as React from "react"
+
+export default function Poll(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      // width={24}
+      // height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon"
+      {...props}
+    >
+      <path d="M0 0h24v24H0z" stroke="none" />
+      <path d="M3 13a1 1 0 011-1h4a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1zM15 9a1 1 0 011-1h4a1 1 0 011 1v10a1 1 0 01-1 1h-4a1 1 0 01-1-1zM9 5a1 1 0 011-1h4a1 1 0 011 1v14a1 1 0 01-1 1h-4a1 1 0 01-1-1zM4 20h14" />
+    </svg>
+  )
+}

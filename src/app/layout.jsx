16613e05287b79738a243338a0539f82e180e7c8
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-07-31 01:21:28 +0200

    feat:

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,6 @@
 import { Inter } from "next/font/google";
 import "@/css/reset/reset.css";
 import "@/css/globals.css";
-import "@/css/init.css";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        
+        {children}
+        </body>
     </html>
   );
 }

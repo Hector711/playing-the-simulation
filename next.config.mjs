7224d43a4ba87a5571ff3e311d0f9e0008dcb546
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-10-17 21:56:40 +0200

    feat: :tada:

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/css')],
+    additionalData: `@import "@/css/_variables.scss";`,
+  },
+};
+
+export default nextConfig;
+

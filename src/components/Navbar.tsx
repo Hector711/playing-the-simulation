09308d2f44984998439608c1988199fc1d8ee292
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-10-27 02:07:25 +0100

    feat:

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+// import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
   return (
@@ -11,32 +11,32 @@ export default function Navbar() {
           <Link href='/home'>
             <h1 className='main-title'>Playing The Simulation</h1>
           </Link>
-          {navLinks.map((link, i) => (
+          {/* {navLinks.map((link, i) => (
             <NavLinkButton key={i} href={link.page}>
               {link.title}
             </NavLinkButton>
-          ))}
+          ))} */}
         </div>
         <div id='nav-right'>
-          <NavLinkButton href='/comunidad'>Comunidad</NavLinkButton>
+          {/* <NavLinkButton href='/comunidad'>Comunidad</NavLinkButton> */}
         </div>
       </div>
     </nav>
   );
 }
 
-function NavLinkButton({ children, href }: { children: React.ReactNode, href: string }) {
-  const isActive = usePathname() === href;
-  return (
-    <Link href={href} className={isActive ? 'active-link' : 'inactive-link'}>
-      <h2>{children}</h2>
-    </Link>
-  );
-}
+// function NavLinkButton({ children, href }: { children: React.ReactNode, href: string }) {
+//   const isActive = usePathname() === href;
+//   return (
+//     <Link href={href} className={isActive ? 'active-link' : 'inactive-link'}>
+//       <h2>{children}</h2>
+//     </Link>
+//   );
+// }
 
-const navLinks = [
-  {title: 'Classroom', page: '/classroom'},
-  {title: 'Playground', page: '/playground'},
-  {title: 'Members', page: '/members'},
-  {title: 'Biblioteca', page: '/library'},
-]
+// const navLinks = [
+//   {title: 'Classroom', page: '/classroom'},
+//   {title: 'Playground', page: '/playground'},
+//   {title: 'Members', page: '/members'},
+//   {title: 'Biblioteca', page: '/library'},
+// ]

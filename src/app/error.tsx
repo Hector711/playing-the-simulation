c461fb09d449b51e78fadf9413772b68f0a5a598
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-10-29 17:47:00 +0100

    feat: :goal_net: General error.tsx created

---
+++ src/app/error.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useEffect } from 'react';
+
+interface ErrorProps {
+  error: Error;
+  reset: () => void;
+}
+
+export default function Error(props: ErrorProps) {
+  useEffect(() => {
+    // Aqui puedes ejecutar servicios de observabilidad, servicios que monitoriean la aplicación.
+    console.log(props.error);
+  }, [props.error]);
+
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <div>
+        <h1 className='text-2xl font-bold text-red-500'>
+          Ha ocurrido un Error 😢
+        </h1>
+        <button
+          onClick={props.reset}
+          className='mt-4 px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-700 transition duration-300'
+        >
+          Volver a intentarlo
+        </button>
+      </div>
+    </div>
+  );
+}
